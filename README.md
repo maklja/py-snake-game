006e698ef7632a055382da8b041c20a7c8c6b5e0
Bojan Makljenovic <maklja90@gmail.com>
2024-05-30 15:48:04 +0200

    Snake game implementation

---
+++ README.md
@@ -0,0 +1,24 @@
+# Snake Game
+
+This is a simple implementation of the classic Snake game using Python and the Turtle graphics library. The project was created for learning purposes and demonstrates basic concepts of game development, such as handling user input, updating game state, and rendering graphics.
+
+## Installation
+
+To run this game on your local machine, you'll need to have Python installed. You can download Python from the official website: [python.org](https://www.python.org/)
+
+1. Clone this repository to your local machine using:
+   ```bash
+   git clone https://github.com/your-username/snake-game.git
+   ```
+
+## Usage
+
+To start the game, run the following command in your terminal:
+
+```bash
+python ./src/main.py
+```
+
+## License
+
+This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for details.
--- src/main.py
+++ src/main.py
@@ -1,139 +1,202 @@
 from turtle import Turtle, Screen
 from random import randint
-from threading import get_ident
+from math import ceil
 
-def create_tail_part(position, heading = 0):
-    tail_part = Turtle(shape="square", visible=False)
-    tail_part.speed(speed="fastest")
-    tail_part.penup()
-    tail_part.shapesize(0.8, 0.8)
-    tail_part.setheading(heading)
-    tail_part.setpos(position[0], position[1])
 
-    return tail_part
+class SnakeGame:
 
-def move_snake_tail_part(position, heading = 0, tail_part_size = 20):
-    if heading == 0:
-        return (position[0] + tail_part_size, position[1])
-    elif heading == 180:
-        return (position[0] - tail_part_size, position[1])
-    elif heading == 90:
-        return (position[0], position[1] + tail_part_size)
-    else:
-        return (position[0], position[1] - tail_part_size)
+    def __init__(self) -> None:
+        self.score = 0
+        self.snake = []
+        self.screen = Screen()
+        self.screen.setup(600, 600)
+        self.screen.bgcolor("black")
+        self.tail_part_size = 20
+        self.gift = None
+        self.heading = 0
+        self.create_score()
+        for i in range(3):
+            tail_part = self.create_part(
+                self.move_snake_tail_part([i * -1, 0]))
+            self.snake.append(tail_part)
 
-def create_gift(width, height, tail_part_size = 20):
-    half_width = round((width - width * 0.1) / 2)
-    half_height = round((height - height * 0.1) / 2)
-    
-    x = randint(half_width * -1, half_width)
-    y = randint(half_height * -1, half_height)
+    def create_score(self):
+        score_pen = Turtle(visible=False)
+        score_pen.penup()
+        screen_height = self.screen.window_height() / 2
+        score_pen.setpos(0, screen_height - screen_height * 0.1)
+        score_pen.color("white")
+        score_pen.write("Score: 0", move=False, align="center",
+                        font=("Arial", 18, "normal"))
+        self.score_pen = score_pen
 
-    gift = create_tail_part([x - x % tail_part_size, y - y % tail_part_size])
-    gift.showturtle()
-    return gift
+    def update_score(self):
+        self.score += 1
+        self.score_pen.clear()
+        self.score_pen.write(f"Score: {self.score}", move=False, align="center",
+                             font=("Arial", 18, "normal"))
 
-def has_collected_gift(snake, gift):
-    if gift is None:
+    def game_over(self):
+        game_over_pen = Turtle(visible=False)
+        game_over_pen.penup()
+        game_over_pen.color("red")
+        game_over_pen.write("Game over!", move=False, align="center",
+                            font=("Arial", 24, "normal"))
+
+    def move_snake_tail_part(self, position, heading=0):
+        if heading == 0:
+            return (position[0] + self.tail_part_size, position[1])
+        elif heading == 180:
+            return (position[0] - self.tail_part_size, position[1])
+        elif heading == 90:
+            return (position[0], position[1] + self.tail_part_size)
+        else:
+            return (position[0], position[1] - self.tail_part_size)
+
+    def create_part(self, position, heading=0, color="white"):
+        tail_part = Turtle(shape="square", visible=False)
+        tail_part.color(color)
+        tail_part.speed(speed="fastest")
+        tail_part.penup()
+        tail_part.shapesize(0.8, 0.8)
+        tail_part.setheading(heading)
+        tail_part.setpos(position[0], position[1])
+
+        return tail_part
+
+    def create_gift(self, width, height):
+        half_width = round((width - width * 0.1) / 2)
+        half_height = round((height - height * 0.1) / 2)
+
+        x = randint(half_width * -1, half_width)
+        y = randint(half_height * -1, half_height)
+
+        self.gift = self.create_part(
+            position=[x - x % self.tail_part_size, y - y %
+                      self.tail_part_size],
+            color="blue"
+        )
+        self.gift.showturtle()
+
+    def has_collected_gift(self):
+        if self.gift is None:
+            return False
+
+        head_pos = self.snake[0].pos()
+        gift_pos = self.gift.pos()
+        return self.compare_positions(head_pos, gift_pos)
+
+    def compare_positions(self, pos1, pos2):
+        return pos1[0] == pos2[0] and pos1[1] == pos2[1]
+
+    def add_new_tail_part(self):
+        last_tail_part_pos = self.snake[len(self.snake) - 1].pos()
+        new_tail_part_spots = [
+            (last_tail_part_pos[0] +
+             self.tail_part_size, last_tail_part_pos[1]),
+            (last_tail_part_pos[0] -
+             self.tail_part_size, last_tail_part_pos[1]),
+            (last_tail_part_pos[0],
+             last_tail_part_pos[1] + self.tail_part_size),
+            (last_tail_part_pos[0],
+             last_tail_part_pos[1] - self.tail_part_size)
+        ]
+
+        for new_tail_pos in new_tail_part_spots:
+            is_free = True
+            for tail_part in self.snake:
+                tail_part_pos = tail_part.pos()
+                if self.compare_positions(new_tail_pos, tail_part_pos):
+                    is_free = False
+                    break
+
+            if is_free:
+                new_tail_part = self.create_part(new_tail_pos)
+                self.snake.append(new_tail_part)
+                return
+
+    def collision_happened(self):
+        head_pos = self.snake[0].pos()
+        tail = slice(1, len(self.snake))
+
+        half_width = ceil(self.screen.window_width() / 2)
+        half_height = ceil(self.screen.window_height() / 2)
+
+        if head_pos[0] >= half_width or head_pos[0] <= half_width * -1:
+            return True
+
+        if head_pos[1] >= half_height or head_pos[1] <= half_height * -1:
+            return True
+
+        for tail_part in self.snake[tail]:
+            if self.compare_positions(head_pos, tail_part.pos()):
+                return True
+
         return False
-    
-    head_pos = snake[0].pos()
-    gift_pos = gift.pos()
-    return head_pos[0] == gift_pos[0] and head_pos[1] == gift_pos[1]
 
-def add_new_tail_part(snake, tail_part_size = 20):
-    last_tail_part_pos = snake[len(snake) - 1].pos()
-    new_tail_part_spots = [
-        (last_tail_part_pos[0] + tail_part_size, last_tail_part_pos[1]),
-        (last_tail_part_pos[0] - tail_part_size, last_tail_part_pos[1]),
-        (last_tail_part_pos[0], last_tail_part_pos[1] + tail_part_size),
-        (last_tail_part_pos[0], last_tail_part_pos[1] - tail_part_size)
-    ]
-    
-    for new_tail_pos in new_tail_part_spots:
-        is_free = True
-        for tail_part in snake:
-            tail_part_pos = tail_part.pos()
-            if new_tail_pos[0] == tail_part_pos[0] and new_tail_pos[1] == tail_part_pos[1]:
-                is_free = False
-                break
-            
-        if is_free:
-            new_tail_part = create_tail_part(new_tail_pos)
-            snake.append(new_tail_part)
+    def handle_move_up(self):
+        if self.heading == 270:
             return
-  
+        self.heading = 90
 
-def render_loop(screen, snake):
-    
-    for tail_part in snake:
-        tail_part.showturtle()
-    
-    def handle_move_up():
-        print('UP ' + str(get_ident()))
-        head = snake[0]
-        if head.heading() == 270:
+    def handle_move_down(self):
+        if self.heading == 90:
             return
-        head.setheading(90)
-        
-    def handle_move_down():
-        print('DOWN ' + str(get_ident()))
-        head = snake[0]
-        if head.heading() == 90:
+        self.heading = 270
+
+    def handle_move_left(self):
+        if self.heading == 0:
             return
-        head.setheading(270)
-        
-    def handle_move_left():
-        print('LEFT ' + str(get_ident()))
-        head = snake[0]
-        if head.heading() == 0:
+        self.heading = 180
+
+    def handle_move_right(self):
+        if self.heading == 180:
             return
-        head.setheading(180)
-        
-    def handle_move_right():
-        print('RIGHT ' + str(get_ident()))
-        head = snake[0]
-        if head.heading() == 180:
-            return
-        head.setheading(0)
-        
-    screen.onkeypress(handle_move_up, "w")
-    screen.onkeypress(handle_move_down, "s")
-    screen.onkeypress(handle_move_left, "a")
-    screen.onkeypress(handle_move_right, "d")
-    
-    def next_frame(gift = None):
-        print("Frame " + str(get_ident()))
-        if gift is None:
-            gift = create_gift(screen.window_width(), screen.window_height())
-            
-        head = snake[0]
-        new_tail_part = create_tail_part(move_snake_tail_part(head.pos(), head.heading()), head.heading())
-        snake.insert(0, new_tail_part)
-        
-        last_tail_part = snake.pop()
+        self.heading = 0
+
+    def start(self):
+        for tail_part in self.snake:
+            tail_part.showturtle()
+
+        self.screen.onkeypress(self.handle_move_up, "w")
+        self.screen.onkeypress(self.handle_move_down, "s")
+        self.screen.onkeypress(self.handle_move_left, "a")
+        self.screen.onkeypress(self.handle_move_right, "d")
+
+        self.create_gift(self.screen.window_width(),
+                         self.screen.window_height())
+
+        self.next_frame()
+
+        self.screen.listen()
+        self.screen.exitonclick()
+
+    def next_frame(self):
+        head = self.snake[0]
+        new_tail_part = self.create_part(
+            self.move_snake_tail_part(head.pos(), self.heading), self.heading
+        )
+        self.snake.insert(0, new_tail_part)
+
+        last_tail_part = self.snake.pop()
         last_tail_part.hideturtle()
         last_tail_part.clear()
         new_tail_part.showturtle()
-        
-        if has_collected_gift(snake, gift):
-            add_new_tail_part(snake)
-            gift.hideturtle()
-            gift.clear()
-            gift = create_gift(screen.window_width(), screen.window_height())
-        
-        screen.ontimer(lambda: next_frame(gift), 100)
-            
-    next_frame()
 
-snake = []
-screen = Screen()
+        if self.collision_happened():
+            self.game_over()
+            return
 
-for i in range(3):
-    tail_part = create_tail_part(move_snake_tail_part([i * -1, 0]))
-    snake.append(tail_part)
+        if self.has_collected_gift():
+            self.update_score()
+            self.add_new_tail_part()
+            self.gift.hideturtle()
+            self.gift.clear()
+            self.create_gift(
+                self.screen.window_width(), self.screen.window_height())
 
-render_loop(screen, snake)
+        self.screen.ontimer(lambda: self.next_frame(), 100)
 
-screen.listen()
-screen.exitonclick()
+
+game = SnakeGame()
+game.start()
